2f0fcb352db725bb7233e8d9182ff686299a88c0
auamarasena <anudaumavin2@gmail.com>
2025-07-31 20:22:06 +0530

    Initializing frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+const app = express();
+app.listen(5001, () => {
+  console.log("Server started on port 5001");
+});
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>My New Referral Project</p>
+    </div>
+  );
+};
+
+export default Home;
