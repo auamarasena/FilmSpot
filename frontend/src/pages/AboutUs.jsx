f7ddecdc5ae361e470220782b292982de935cc44
auamarasena <anudaumavin2@gmail.com>
2025-08-02 11:36:05 +0530

    add AboutUs Page

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutUs from "../components/AboutUs/About";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  );
+};
+
+export default AboutUs;
