b3406ae692bb66fc7a688046493a7f373ab9a55a
auamarasena <anudaumavin2@gmail.com>
2025-08-02 11:34:26 +0530

    add movie shwing page

---
+++ frontend/src/pages/Moviepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../components/MovieList/Movies.css";
+import Movies from "../components/MovieList/Movies";
+
+const Moviepage = () => {
+  return (
+    <div>
+      <Movies />
+    </div>
+  );
+};
+
+export default Moviepage;
