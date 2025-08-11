f7a2c46ea4027c40639f6ca485e194cfc233cc51
auamarasena <anudaumavin2@gmail.com>
2025-08-11 22:53:00 +0530

    starting backend

--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,5 @@ import express from "express";
 
 const app = express();
 app.listen(5001, () => {
-  console.log("Server started on port 5001");
+  console.log("Server Connected and started on port 5001 ");
 });
