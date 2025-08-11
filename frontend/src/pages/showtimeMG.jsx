f6163a4f75b258781d7b6791d133e85e60e9e569
auamarasena <anudaumavin2@gmail.com>
2025-08-11 22:45:17 +0530

    add page

---
+++ frontend/src/pages/showtimeMG.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShowtimeMG from '../components/ShowtimeMG/ShowtimeMG';
+
+const ShowtimeMGpage = () => {
+  return (
+    <div>
+      <ShowtimeMG/>
+    </div>
+  )
+}
+
+export default ShowtimeMGpage;
