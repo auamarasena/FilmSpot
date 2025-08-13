7d62aec2f4717da0092b11aa25558354ea4a2647
auamarasena <anudaumavin2@gmail.com>
2025-08-13 22:27:46 +0530

    add auth realated backend

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      //Get the token from the header (Bearer Token)
+      token = req.headers.authorization.split(" ")[1];
+
+      //Verify the token using your JWT_SECRET
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      //Find the user in the database using the ID from the token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      //Call the next middleware or controller
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
+
+export { protect };
