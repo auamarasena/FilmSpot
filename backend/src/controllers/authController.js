5081e6289a40921f5c49a519be25533339c8e9e8
auamarasena <anudaumavin2@gmail.com>
2025-08-13 22:09:48 +0530

    add authcontroller

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,117 @@
+import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+//Generate a JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+//Register a new user
+//POST /api/auth/register
+export const registerUser = async (req, res) => {
+  const { firstName, lastName, email, password, mobile } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ message: "User with that email already exists" });
+    }
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+      mobile,
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        firstName: user.firstName,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+//Authenticate user & get token
+//POST /api/auth/login
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        firstName: user.firstName,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+//Get user details by ID
+//GET /api/auth/users/:id
+export const getUserDetails = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const { password, ...userDetails } = user.toObject();
+    res.status(200).json(userDetails);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching user details", error: error.message });
+  }
+};
+
+//Get total user count for admin dashboard
+//GET /api/auth/count
+export const getTotalUserCount = async (req, res) => {
+  try {
+    const count = await User.countDocuments();
+    res.json({ count });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+//Get user profile
+//GET /api/auth/profile
+//Private
+export const getUserProfile = async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      mobile: user.mobile,
+    });
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+};
