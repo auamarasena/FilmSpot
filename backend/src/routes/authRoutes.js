770b48d2bf4145f9575f15acdd47cfbbfcf5ae6e
auamarasena <anudaumavin2@gmail.com>
2025-08-14 23:27:55 +0530

    add auth routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,15 +3,17 @@ import {
   registerUser,
   loginUser,
   getUserProfile,
+  getTotalUserCount,
 } from "../controllers/authController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
-//Protected routes
 router.get("/profile", protect, getUserProfile);
+
+//GET /api/auth/count
+router.get("/count", protect, admin, getTotalUserCount);
 
 export default router;
