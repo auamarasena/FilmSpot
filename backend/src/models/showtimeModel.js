ac8f068eb2049cc72f02fa5c596fbba7ebb32d17
auamarasena <anudaumavin2@gmail.com>
2025-08-17 11:32:31 +0530

    add showtime model

---
+++ backend/src/models/showtimeModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const showtimeSchema = new mongoose.Schema({
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Movie",
+    required: true,
+  },
+  screenId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Screens",
+    required: true,
+  },
+  start_date: { type: Date, required: true },
+  start_time: { type: String, required: true },
+  seatPrice: { type: Number, required: true },
+  recurrence: {
+    type: { type: String, enum: ["none", "daily"], default: "none" }, 
+    endDate: { type: Date },
+  },
+});
+
+const Showtime = mongoose.model("Showtime", showtimeSchema);
+
+
+export default Showtime;
